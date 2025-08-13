4226d8f246209cbc7bd870763dc86009f3c97ad9
Raquel Moss <raquelxmoss@gmail.com>
2025-08-13 18:36:46 +0900

    Attempt to appease reddit...

--- lib/reddit.rb
+++ lib/reddit.rb
@@ -8,14 +8,48 @@ module Reddit
   def self.search_threads
     q   = "title:%22Monthly%20Book%20Recommendation%20Thread%22"
     url = "https://www.reddit.com/r/#{SUB}/search.json?restrict_sr=on&sort=new&q=#{q}"
-    res = HTTParty.get(url, headers: {"User-Agent" => ENV.fetch("REDDIT_USER_AGENT", "md-curator")}).parsed_response
+    
+    headers = {
+      "User-Agent" => ENV.fetch("REDDIT_USER_AGENT", "reading-list-curator/1.0 (by /u/raquelxmoss)"),
+      "Accept" => "application/json",
+      "Accept-Language" => "en-US,en;q=0.9",
+      "Cache-Control" => "no-cache"
+    }
+    
+    response = HTTParty.get(url, headers: headers)
+    
+    # Check if we got blocked (HTML response instead of JSON)
+    if response.headers["content-type"]&.include?("text/html")
+      puts "Warning: Reddit blocked the request. Response: #{response.body[0..200]}"
+      return []
+    end
+    
+    res = response.parsed_response
+    return [] unless res.is_a?(Hash)
+    
     posts = res.dig("data", "children") || []
     posts.map { |p| p.dig("data", "permalink") }.compact.uniq
   end
 
   def self.fetch_comments(permalink)
     url = "https://www.reddit.com#{permalink}.json?sort=top"
-    res = HTTParty.get(url, headers: {"User-Agent" => ENV.fetch("REDDIT_USER_AGENT", "md-curator")}).parsed_response
+    
+    headers = {
+      "User-Agent" => ENV.fetch("REDDIT_USER_AGENT", "reading-list-curator/1.0 (by /u/raquelxmoss)"),
+      "Accept" => "application/json",
+      "Accept-Language" => "en-US,en;q=0.9",
+      "Cache-Control" => "no-cache"
+    }
+    
+    response = HTTParty.get(url, headers: headers)
+    
+    # Check if we got blocked
+    if response.headers["content-type"]&.include?("text/html")
+      puts "Warning: Reddit blocked the request for comments"
+      return []
+    end
+    
+    res = response.parsed_response
     return [] unless res.is_a?(Array)
     tree = res[1].dig("data","children") || []
     comments = flatten_comments(tree)
